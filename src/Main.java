760eea142f200aa6bf972a25b3aaec6a5131db84
SofiSolo <sofa.galaxia@yandex.ru>
2022-08-24 15:27:29 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,18 @@ public class Main {
         paper = paper + 4;
         System.out.println(paper);
 
+        var dog = 8.0;
+       dog = dog - 3.5;
+       System.out.println(dog);
+       var cat = 3.6;
+      cat = cat -3.5;
+      System.out.println(cat);
+      var paper= 763789.0;
+      paper = paper -3.5;
+      System.out.println(paper);
+
+
+
 
 
         System.out.println("Hello world!");
