fb9e4b2881a6b93a5ebc2b1cd4e53426e3c1355d
SofiSolo <sofa.galaxia@yandex.ru>
2022-08-24 15:32:25 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8;
+        var dog = 8.0;
         System.out.println(dog);
         var cat = 3.6;
         System.out.println(cat);
@@ -14,14 +14,14 @@ public class Main {
         paper = paper + 4;
         System.out.println(paper);
 
-        var dog = 8.0;
+
        dog = dog - 3.5;
        System.out.println(dog);
-       var cat = 3.6;
-      cat = cat -3.5;
+
+      cat = cat -1.6;
       System.out.println(cat);
-      var paper= 763789.0;
-      paper = paper -3.5;
+
+      paper = paper -7639;
       System.out.println(paper);
 
 
